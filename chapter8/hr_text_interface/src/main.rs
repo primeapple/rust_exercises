7220691c19b118bba503e617870557469753470d
primeapple <toni.mueller.web@mailbox.org>
2022-08-10 22:28:34 +0200

    added hr text interface

---
+++ chapter8/hr_text_interface/src/main.rs
@@ -0,0 +1,58 @@
+use std::{io, collections::HashMap};
+
+enum Action {
+    Add(String, String),
+    List(String),
+    Quit,
+    Unknown
+}
+
+fn main() {
+    let mut map = HashMap::<String, Vec<String>>::new();
+    loop {
+        let action = next_action();
+        match action {
+            Action::Add(employee, department) => {
+                let employees = map.entry(department).or_insert(Vec::new());
+                employees.push(employee);
+                employees.sort_unstable();
+            },
+            Action::List(department) => {
+                let employees = map.get(&department);
+                if let Some(employees) = employees  {
+                    println!("Department '{}' has employees {:?}", department, employees);
+                } else {
+                    println!("Department '{}' has has no employees yest", department);
+                }
+            },
+            Action::Quit => {
+                break;
+            },
+            Action::Unknown => {}
+        }
+    }
+}
+
+fn next_action() -> Action {
+    let mut input = String::new();
+    println!();
+    println!("What do you want to do?");
+    println!("a $PERSON $DEPARTMENT => adds $PERSON to $DEPARTMENT");
+    println!("l $DEPARTMENT         => lists all members of $DEPARTMENT in alphabetic order");
+    println!("q                     => quits the app");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line!");
+    input_to_action(input)
+}
+
+fn input_to_action(s: String) -> Action {
+    let words: Vec<&str> = s.split_whitespace().collect();
+    match words.as_slice() {
+        [ "a", name, department ] =>  Action::Add(name.to_string(), department.to_string()),
+        [ "l", department ] => Action::List(department.to_string()),
+        [ "q" ] => Action::Quit,
+        _ => Action::Unknown
+    }
+}
+
